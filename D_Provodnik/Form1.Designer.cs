ca3dca2e1d0534bd483bccf84cb2033ef1f4f5ee
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-03-03 09:58:26 +0200

    Добавьте файлы проекта.

---
+++ D_Provodnik/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace D_Provodnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeProgramToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.createFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(23, 105);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(219, 408);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 39);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Инструкция \r\nДвойным шелчком выбрать \r\nдиск и папку\r\n";
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(271, 105);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(505, 408);
+            this.treeView2.TabIndex = 14;
+            this.treeView2.DoubleClick += new System.EventHandler(this.treeView2_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(753, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 39);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Инструкция \r\nДвойным шелчком выбрать \r\nпапку\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Строка адреса";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeProgramToolStripMenuItem,
+            this.programInfoToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // closeProgramToolStripMenuItem
+            // 
+            this.closeProgramToolStripMenuItem.Name = "closeProgramToolStripMenuItem";
+            this.closeProgramToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.closeProgramToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.closeProgramToolStripMenuItem.Text = "Close program";
+            this.closeProgramToolStripMenuItem.Click += new System.EventHandler(this.closeProgramToolStripMenuItem_Click);
+            // 
+            // programInfoToolStripMenuItem
+            // 
+            this.programInfoToolStripMenuItem.Name = "programInfoToolStripMenuItem";
+            this.programInfoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.programInfoToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.programInfoToolStripMenuItem.Text = "Program info";
+            this.programInfoToolStripMenuItem.Click += new System.EventHandler(this.programInfoToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeProgramToolStripMenuItem1,
+            this.createFolderToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // closeProgramToolStripMenuItem1
+            // 
+            this.closeProgramToolStripMenuItem1.Name = "closeProgramToolStripMenuItem1";
+            this.closeProgramToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.closeProgramToolStripMenuItem1.Text = "Close program";
+            this.closeProgramToolStripMenuItem1.Click += new System.EventHandler(this.closeProgramToolStripMenuItem1_Click);
+            // 
+            // createFolderToolStripMenuItem
+            // 
+            this.createFolderToolStripMenuItem.Name = "createFolderToolStripMenuItem";
+            this.createFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createFolderToolStripMenuItem.Text = "Create folder";
+            this.createFolderToolStripMenuItem.Click += new System.EventHandler(this.createFolderToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 524);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Проводник";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeProgramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programInfoToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeProgramToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem createFolderToolStripMenuItem;
+    }
+}
+
---
+++ D_Provodnik/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Provodnik
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode driveNode;
+        private string adres;
+        private string[] folder;
+        public Form1()
+        {
+            InitializeComponent();
+            adres = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DriveInfo driveInfo in DriveInfo.GetDrives())
+            {
+                driveNode = new TreeNode();
+                driveNode.Text = driveInfo.Name;
+                driveNode.Name = driveInfo.Name;
+                //добавляем диски
+                this.treeView1.Nodes.Add(driveNode);
+
+                folder = Directory.GetDirectories(driveInfo.Name);
+                //добавляем папки 
+                for (int i = 0; i < folder.Length; i++)
+                {
+                    this.treeView1.Nodes[driveNode.Name].Nodes.Add(folder[i]);
+
+                }
+
+            }
+        }
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            this.treeView2.Nodes.Clear();
+
+            string path = this.treeView1.SelectedNode.Text;
+            //добаляем в текст бокс полный путь
+            adres = path;
+            this.textBox1.Text = adres;
+
+            string[] files = Directory.GetFiles(path);
+            string[] folder2 = Directory.GetDirectories(path);
+
+            this.treeView2.Nodes.Add("РАЗДЕЛ ПАПКИ");
+            if (folder2.Length == 0)
+            {
+                this.treeView2.Nodes.Add("Папки отсутвуют");
+            }
+            else
+            {
+                for (int i = 0; i < folder2.Length; i++)
+                {
+                    this.treeView2.Nodes.Add(folder2[i]);
+
+                }
+            }
+
+
+            string[] file = Directory.GetFiles(path);
+            this.treeView2.Nodes.Add("РАЗДЕЛ ФАЙЛЫ");
+            if (file.Length == 0)
+            {
+                this.treeView2.Nodes.Add("Файлы отсутвуют");
+            }
+            else
+            {
+                for (int i = 0; i < file.Length; i++)
+                {
+                    this.treeView2.Nodes.Add(file[i]);
+                }
+            }
+        }
+
+        private void treeView2_DoubleClick(object sender, EventArgs e)
+        {
+            string path2 = this.treeView2.SelectedNode.Text;
+            //заносим в текст бокс адрес
+            this.adres = path2;
+            this.textBox1.Text = adres;
+            string[] files3 = Directory.GetFiles(path2);
+            string[] folder3 = Directory.GetDirectories(path2);
+            this.treeView2.Nodes.Clear();
+            this.treeView2.Nodes.Add("РАЗДЕЛ ПАПКИ");
+            if (folder3.Length == 0)
+            {
+                this.treeView2.Nodes.Add("Папки отсутвуют");
+            }
+            else
+            {
+                for (int i = 0; i < folder3.Length; i++)
+                {
+                    this.treeView2.Nodes.Add(folder3[i]);
+
+                }
+            }
+
+
+            string[] file = Directory.GetFiles(path2);
+            this.treeView2.Nodes.Add("РАЗДЕЛ ФАЙЛЫ");
+            if (file.Length == 0)
+            {
+                this.treeView2.Nodes.Add("Файлы отсутвуют");
+            }
+            else
+            {
+                for (int i = 0; i < files3.Length; i++)
+                {
+                    this.treeView2.Nodes.Add(file[i]);
+                }
+            }
+        }
+
+        private void closeProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void programInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Property of Kudriashov Anton\nCreated 15.11.2020\nHome work #7", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void closeProgramToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void createFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //создаем папку
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            string adres2 = folderBrowserDialog.SelectedPath;
+            if (adres2!=string.Empty)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(adres2);
+
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+                //заносим в текст бокс адрес
+                //если папка создана в той дериктори которое отображает второе окно то отображаем изменение
+                //НЕМНОГО ЗАКОЛХОЗИЛ НАДО БЫЛО ЭТО СДЕЛАТЬ ОТДЕЛЬНЫМ МЕТОДОМ ЧТО БЫ НЕ ПЕРЕПИСЫВАТЬ КОД НО ДЛЯ ПРИМЕРА СОЙДЕТ
+                this.textBox1.Text = adres;
+                string[] files3 = Directory.GetFiles(adres);
+                string[] folder3 = Directory.GetDirectories(adres);
+                this.treeView2.Nodes.Clear();
+                this.treeView2.Nodes.Add("РАЗДЕЛ ПАПКИ");
+                if (folder3.Length == 0)
+                {
+                    this.treeView2.Nodes.Add("Папки отсутвуют");
+                }
+                else
+                {
+                    for (int i = 0; i < folder3.Length; i++)
+                    {
+                        this.treeView2.Nodes.Add(folder3[i]);
+
+                    }
+                }
+
+
+                string[] file = Directory.GetFiles(adres);
+                this.treeView2.Nodes.Add("РАЗДЕЛ ФАЙЛЫ");
+                if (file.Length == 0)
+                {
+                    this.treeView2.Nodes.Add("Файлы отсутвуют");
+                }
+                else
+                {
+                    for (int i = 0; i < files3.Length; i++)
+                    {
+                        this.treeView2.Nodes.Add(file[i]);
+                    }
+                }
+            }
+          
+            
+            
+
+
+           
+        }
+    }
+}
